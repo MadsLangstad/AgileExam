2ea9ad082d883c798fb55627ec0bca77df64aaf7
Mads Emil Langstad <langstad.dev@proton.me>
2024-05-27 11:56:20 +0200

    Added tailwind

---
+++ Api/Interfaces/IAnnouncementCard.cs
@@ -0,0 +1,1 @@
+namespace AgileExam.Interfaces;
---
+++ Api/Interfaces/IWeatherCard.cs
@@ -0,0 +1,15 @@
+namespace AgileExam.Interfaces;
+
+public interface IWeatherCard
+{
+    public string City { get; set; }
+    public string Country { get; set; }
+    public string Description { get; set; }
+    public string Temperature { get; set; }
+    public string Humidity { get; set; }
+    public string Pressure { get; set; }
+    public string Windspeed { get; set; }
+    public string Rain { get; set; }
+    public string Clouds { get; set; }
+    public string Icon { get; set; }
+}
