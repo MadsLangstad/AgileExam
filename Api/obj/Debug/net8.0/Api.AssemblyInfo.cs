0d2aa13154361a0d86eb1de4e0358395d333d6a7
Mads Emil Langstad <langstad.dev@proton.me>
2024-05-27 11:57:08 +0200

    added tailwind

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0eb177d9ddfeb19bbcec93f6a81edf0505119b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41015fde22cbe5f3c4709d645057185725775fd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
