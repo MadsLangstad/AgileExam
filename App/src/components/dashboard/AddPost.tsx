a2caf2aff4d0a2f41a2b55d31cae96674f1a726d
Ahrow <jonasbergenheim90@gmail.com>
2024-05-28 16:47:33 +0200

    add modal + upload button

--- App/src/components/dashboard/AddPost.tsx
+++ App/src/components/dashboard/AddPost.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
+import UploadModal from "./UploadModal";
+
 const AddPost = () => {
-  const handleMediaClick = () => {
-    // Add function to add media
-    console.log("Media button clicked");
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const handleButtonClick = () => {
+    setModalVisible(true);
   };
 
-  const handleBirthdayClick = () => {
-    // Add function to add birthday post
-    console.log("Birthday button clicked");
+  const handleCloseModal = () => {
+    setModalVisible(false);
   };
+
   return (
     <div className="p-6">
+      <UploadModal isVisible={isModalVisible} onClose={handleCloseModal} />
       <div className="flex gap-4 justify-center items-center">
         <h1 className="font-semibold text-5xl text-blue-700">Add To Queue</h1>
         <div className="h-10 w-10 bg-[url('/tooltip_icon.svg')] bg-contain bg-no-repeat bg-center"></div>
@@ -18,17 +23,14 @@ const AddPost = () => {
         <div className="mt-20">
           <button
             className="h-36 w-52 rounded-md bg-[url('/img_video_icon.svg')] bg-contain bg-no-repeat bg-center"
-            onClick={handleMediaClick}
+            onClick={handleButtonClick}
           ></button>
           <div className="text-center">
             <p className="text-blue-600 text-xl">Add Image/Video</p>
           </div>
         </div>
         <div className="mt-20">
-          <button
-            className="h-36 w-52 rounded-md bg-[url('/birthday_icon.svg')] bg-contain bg-no-repeat bg-center"
-            onClick={handleBirthdayClick}
-          ></button>
+          <button className="h-36 w-52 rounded-md bg-[url('/birthday_icon.svg')] bg-contain bg-no-repeat bg-center"></button>
           <div className="text-center">
             <p className="text-blue-600 text-xl">Add Birthday Post</p>
           </div>
