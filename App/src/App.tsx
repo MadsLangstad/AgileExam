3525b30f023ddb72d8ffc89e50e4be0acd1a9818
AndersB <Andersbbeil@gmail.com>
2024-05-28 11:01:36 +0200

    LoginPage
    LoginPage update

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -3,7 +3,9 @@ import { LogInPage } from "./pages";
 function App() {
   return (
     <>
+      {/*
       <h1 className="text-pink-700">Agile Exam</h1>
+      */}
       <LogInPage></LogInPage>
     </>
   );
