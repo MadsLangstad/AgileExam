1db55db3cc9f29d24cc722f6ad97fb51ea226898
Ahrow <jonasbergenheim90@gmail.com>
2024-05-28 15:25:48 +0200

    simple layout of the dashboard

--- App/src/pages/DashboardPage.tsx
+++ App/src/pages/DashboardPage.tsx
@@ -1,7 +1,15 @@
 const DashBoardPage = () => {
   return (
     <div>
-      <h1>DashBoard Page</h1>
+      <div className="h-40 w-full bg-red-400">
+        <h1>DashBoard Page | HEADER GOES HERE</h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-screen">
+        <div className="bg-gray-200">Now showing</div>
+        <div className="bg-gray-200">Queue</div>
+        <div className="bg-gray-200">Add new post</div>
+        <div className="bg-gray-200">Make Announcement</div>
+      </div>
     </div>
   );
 };
