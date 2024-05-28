3e85878b75b642ac8a53e3e93eb3c6deb4cba3d8
Sara Adamson <112488580+WarpStarSara@users.noreply.github.com>
2024-05-28 12:57:29 +0200

    First part of the Navigation Bar

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,8 +1,11 @@
 import { LogInPage } from "./pages";
-
+import Navbar from "./components/shared/NavbarNavigation";
 function App() {
   return (
     <>
+      <div className="App">
+        <Navbar />
+      </div>
       <h1 className="text-pink-700">Agile Exam</h1>
       <LogInPage></LogInPage>
     </>
