74b6b6c980a3ff58166303ab9bef624f7557861e
Sara Adamson <112488580+WarpStarSara@users.noreply.github.com>
2024-05-29 14:45:52 +0200

    ADDED Completed navbar

--- App/src/routing/RoutingMain.tsx
+++ App/src/routing/RoutingMain.tsx
@@ -4,11 +4,14 @@ import { LogInPage } from "../pages";
 import { WeatherCardProvider } from "../contexts/WeatherCardContext.tsx";
 import { MediaCardProvider } from "../contexts/MediaCardContext.tsx";
 import { BirthdayCardProvider } from "../contexts/BirthdayCardContext.tsx";
+import Navbar from "../components/shared/NavbarNavigation.tsx";
 
 const RoutingMain = () => {
   return (
     <BrowserRouter>
-      <header className="container-fluid px-0">/* Her kommer navbar */</header>
+      <header className="container-fluid px-0">
+        <Navbar />
+      </header>
       <main className="container">
         <WeatherCardProvider>
           <MediaCardProvider>
