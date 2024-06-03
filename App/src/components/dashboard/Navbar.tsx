0f4a61adc30734c842b1e15686b5da04bc0b80ec
Ahrow <jonasbergenheim90@gmail.com>
2024-06-03 18:57:31 +0200

    small cleanup
    - removed duplicate css
    - remove hidden icons on small screen
    - replaced inline style with tailwind
    - fix logo on smaller screen size

--- App/src/components/dashboard/Navbar.tsx
+++ App/src/components/dashboard/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           {/* Logo on the left */}
-          <div className="flex-shrink-0" style={{ marginLeft: "-8rem" }}>
+          <div className="flex-shrink-0">
             <img
               src="/KPMG_logo_white.png"
               alt="KPMG Logo"
@@ -22,46 +22,39 @@ const Navbar: React.FC = () => {
 
           {/* Spacer to push items to the right */}
           <div className="flex-1 flex justify-end items-center">
-            <div className="hidden sm:flex space-x-4 relative">
+            <div className="flex space-x-4 relative">
               <button
                 onClick={toggleDropdown}
                 className="text-white hover:bg-blue-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium flex items-center space-x-2 focus:outline-none"
               >
                 {" "}
                 Welcome [name] !
-                <img
-                  src="/user.png"
-                  alt="KPMG Logo"
-                  className="h-6 w-6"
-                  style={{ marginLeft: "10px" }}
-                />
+                <img src="/user.png" alt="KPMG Logo" className="h-6 w-6 ml-3" />
                 {isOpen && (
                   <div className="absolute right-0 mt-40 w-44 bg-white shadow-lg z-2">
                     <div className="relative">
-                      <div className="absolute -top-1 right-20 transform rotate-45 w-3 h-3 bg-white border-t border-l border-t border-teal-400"></div>
+                      <div className="absolute -top-1 right-20 transform rotate-45 w-3 h-3 bg-white border-teal-400"></div>
                       <div className="border-t border-teal-400"></div>
                       <a
                         href="App\src\pages\HistoryPage.tsx"
-                        className="block px-4 py-2 text-sm text-blue-800 hover:bg-gray-100 flex items-center space-x-2 focus:outline-none"
+                        className=" px-4 py-2 text-sm text-blue-800 hover:bg-gray-100 flex items-center space-x-2 focus:outline-none"
                       >
                         {" "}
                         <img
                           src="/History.png"
                           alt="A history icon"
-                          className="h-5 w-5"
-                          style={{ marginRight: "10px" }}
+                          className="h-5 w-5 mr-3"
                         />
                         History
                       </a>
                       <a
                         href="App\src\pages\LogInPage.tsx"
-                        className="block px-4 py-2 text-sm text-blue-800 hover:bg-gray-100 border-t border-blue-800 flex items-center space-x-2 focus:outline-none"
+                        className="px-4 py-2 text-sm text-blue-800 hover:bg-gray-100 border-t border-blue-800 flex items-center space-x-2 focus:outline-none"
                       >
                         <img
                           src="/logout.png"
                           alt="A logout icon"
-                          className="h-5 w-5"
-                          style={{ marginRight: "10px" }}
+                          className="h-5 w-5 mr-3"
                         />
                         Log out
                       </a>
