ba00f5e7d5702b92f0d5b64fe7eecafa7e95d829
Sara Adamson <112488580+WarpStarSara@users.noreply.github.com>
2024-06-03 14:21:53 +0200

    ADDED Custom colors to tailwind config

--- App/tailwind.config.js
+++ App/tailwind.config.js
@@ -8,4 +8,16 @@ export default {
     extend: {},
   },
   plugins: [],
+
+  theme: {
+    colors: {
+      KPMGBlue: "#00338D",
+      MediumBlue: "#005EB8",
+      LightBlue: "#0091DA",
+      Violet: "#483698",
+      Purple: "#470A68",
+      LightPurple: "#6D2077",
+      Green: "#00A3A1",
+    },
+  },
 };
